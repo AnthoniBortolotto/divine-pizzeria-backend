af51c461e13eff62b92658621d71790784a31b4c
AnthoniBortolotto <antho@outlook.com.br>
2025-05-28 13:50:04 -0300

    validators - added IsDateAfterNow custom validation

--- utils/validator.go
+++ utils/validator.go
@@ -1,7 +1,9 @@
 package utils_validator
 
 import (
+	"reflect"
 	"regexp"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -18,4 +20,20 @@ func IsCep(fl validator.FieldLevel) bool {
 
 	cepRegex := regexp.MustCompile(regex)
 	return cepRegex.MatchString(fl.Field().String())
+}
+
+func IsDateAfterNow(fl validator.FieldLevel) bool {
+
+	if fl.Field().Type() != reflect.TypeOf(time.Time{}) {
+		println("Expected time.Time type, got:", fl.Field().Type().String())
+		return false
+	}
+	// Tenta fazer o parse da string no formato RFC3339
+	inputTime := fl.Field().Interface().(time.Time)
+	// Compara com o horário atual
+	if inputTime.After(time.Now()) {
+		return true
+	}
+
+	return false
 }
